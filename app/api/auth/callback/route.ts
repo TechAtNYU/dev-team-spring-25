f17a772925cd85cd328c92a90e60042b7d9be302
chenxin-yan <cxyan04@gmail.com>
2025-03-28 22:51:05 -0400

    move auth callback into `/api` dir

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -21,11 +21,8 @@ export async function GET(request: Request) {
     if (!error) {
       // User has an allowed email domain, proceed
       const forwardedHost = request.headers.get("x-forwarded-host");
-      const isLocalEnv = process.env.NODE_ENV === "development";
 
-      if (isLocalEnv) {
-        return NextResponse.redirect(`${origin}${next}`);
-      } else if (forwardedHost) {
+      if (forwardedHost) {
         return NextResponse.redirect(`https://${forwardedHost}${next}`);
       } else {
         return NextResponse.redirect(`${origin}${next}`);
